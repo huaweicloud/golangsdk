d2b4e9028c718039bc29147a6c4326b6b5b2a7db
zhongjun2 <jun.zhongjun2@gmail.com>
2018-03-15 16:27:23 +0800

    Update client_extension.go

--- openstack/client_extension.go
+++ openstack/client_extension.go
@@ -96,7 +96,12 @@ func NewAutoScalingService(client *golangsdk.ProviderClient, eo golangsdk.Endpoi
 // NewKmsKeyV1 creates a ServiceClient that may be used to access the v1
 // kms key service.
 func NewKmsKeyV1(client *golangsdk.ProviderClient, eo golangsdk.EndpointOpts) (*golangsdk.ServiceClient, error) {
-	sc, err := initClientOpts(client, eo, "kms")
+	sc, err := initClientOpts(client, eo, "compute")
+	sc.Endpoint = strings.Replace(sc.Endpoint, "ecs", "kms", 1)
+	sc.Endpoint = strings.Replace(sc.Endpoint, "v2", "v1.0", 1)
+	sc.ResourceBase = sc.Endpoint + "kms/"
+	sc.Type = "kms"
+
 	return sc, err
 }
 
@@ -116,12 +121,8 @@ func NewElasticLoadBalancer(client *golangsdk.ProviderClient, eo golangsdk.Endpo
 //NewVpcServiceV1 creates the a ServiceClient that may be used to access the v1
 //vpc service which is a service of public ip management of huawei cloud
 func NewVpcServiceV1(client *golangsdk.ProviderClient, eo golangsdk.EndpointOpts) (*golangsdk.ServiceClient, error) {
-	sc, err := initClientOpts(client, eo, "compute")
-        sc.Endpoint = strings.Replace(sc.Endpoint, "ecs", "kms", 1)
-        sc.Endpoint = strings.Replace(sc.Endpoint, "v2", "v1.0", 1)
-        sc.ResourceBase = sc.Endpoint + "kms/"
-        sc.Type = "kms"
-        return sc, err
+	sc, err := initClientOpts(client, eo, "network")
+	return sc, err
 }
 
 // NewNatV2 creates a ServiceClient that may be used with the v2 nat package.
