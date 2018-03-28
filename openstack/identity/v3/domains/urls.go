3a42c8f77f98c1303e4fd0696a0bc1701d1b6298
Chen.Zhidong <njutczd@gmail.com>
2018-03-29 01:20:15 +0800

    Fix domains url path

--- openstack/identity/v3/domains/urls.go
+++ openstack/identity/v3/domains/urls.go
@@ -3,21 +3,21 @@ package domains
 import "github.com/huaweicloud/golangsdk"
 
 func listURL(client *golangsdk.ServiceClient) string {
-	return client.ServiceURL("domains")
+	return client.ServiceURL("auth/domains")
 }
 
 func getURL(client *golangsdk.ServiceClient, domainID string) string {
-	return client.ServiceURL("domains", domainID)
+	return client.ServiceURL("auth/domains", domainID)
 }
 
 func createURL(client *golangsdk.ServiceClient) string {
-	return client.ServiceURL("domains")
+	return client.ServiceURL("auth/domains")
 }
 
 func deleteURL(client *golangsdk.ServiceClient, domainID string) string {
-	return client.ServiceURL("domains", domainID)
+	return client.ServiceURL("auth/domains", domainID)
 }
 
 func updateURL(client *golangsdk.ServiceClient, domainID string) string {
-	return client.ServiceURL("domains", domainID)
+	return client.ServiceURL("auth/domains", domainID)
 }
