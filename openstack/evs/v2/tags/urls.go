f90d95674ead542ccda9ac986a4f1be02d87556d
zengchen1024 <chenzeng2@huawei.com>
2018-04-11 14:15:22 +0800

    revise the evs-tag

--- openstack/evs/v2/tags/urls.go
+++ openstack/evs/v2/tags/urls.go
@@ -1,14 +1,13 @@
 package tags
 
 import (
-	"github.com/gophercloud/gophercloud"
+	"github.com/huaweicloud/golangsdk"
 )
 
-func createURL(c *gophercloud.ServiceClient, resource_type, resource_id string) string {
+func createURL(c *golangsdk.ServiceClient, resource_type, resource_id string) string {
 	return c.ServiceURL("os-vendor-tags", resource_type, resource_id)
 }
 
-func getURL(c *gophercloud.ServiceClient, resource_type, resource_id string) string {
+func getURL(c *golangsdk.ServiceClient, resource_type, resource_id string) string {
 	return c.ServiceURL("os-vendor-tags", resource_type, resource_id)
 }
-
