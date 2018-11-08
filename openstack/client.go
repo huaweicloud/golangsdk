60672b79c76a4eec63f32293ce30b4119365e1bc
click2cloud-pikab <43378237+pikab@users.noreply.github.com>
2018-11-08 08:06:44 +0530

    Add Cloud Trace Service Interfaces (#79)
    * add cloud trace service interfaces

--- openstack/client.go
+++ openstack/client.go
@@ -764,6 +764,12 @@ func NewVBS(client *golangsdk.ProviderClient, eo golangsdk.EndpointOpts) (*golan
 	return sc, err
 }
 
+// NewCTSService creates a ServiceClient that can be used to access the Cloud Trace service.
+func NewCTSService(client *golangsdk.ProviderClient, eo golangsdk.EndpointOpts) (*golangsdk.ServiceClient, error) {
+	sc, err := initClientOpts(client, eo, "cts")
+	return sc, err
+}
+
 // NewELBV1 creates a ServiceClient that may be used to access the ELB service.
 func NewELBV1(client *golangsdk.ProviderClient, eo golangsdk.EndpointOpts) (*golangsdk.ServiceClient, error) {
 	sc, err := initClientOpts(client, eo, "elbv1")
