60e6d2e5b148be2836a525a1db1424037705abed
freesky-edward <freesky.edward@gmail.com>
2019-01-21 19:10:34 +0800

    Fix testing bug

--- openstack/identity/v3/groups/testing/fixtures.go
+++ openstack/identity/v3/groups/testing/fixtures.go
@@ -186,7 +186,7 @@ func HandleCreateGroupSuccessfully(t *testing.T) {
 		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
 		th.TestJSONRequest(t, r, CreateRequest)
 
-		w.WriteHeader(http.StatusCreated)
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, GetOutput)
 	})
 }
