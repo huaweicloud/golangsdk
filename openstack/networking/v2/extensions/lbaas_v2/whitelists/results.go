20180b2fbf7844749599a62fd894aa257dc0c155
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-10-12 16:47:55 +0800

    Add LB Whitelist (#190)

---
+++ openstack/networking/v2/extensions/lbaas_v2/whitelists/results.go
@@ -0,0 +1,40 @@
+package whitelists
+
+import (
+	"github.com/huaweicloud/golangsdk"
+)
+
+type Whitelist struct {
+	ID              string `json:"id"`
+	TenantId        string `json:"tenant_id"`
+	ListenerId      string `json:"listener_id"`
+	EnableWhitelist bool   `json:"enable_whitelist"`
+	Whitelist       string `json:"whitelist"`
+}
+
+type commonResult struct {
+	golangsdk.Result
+}
+
+func (r commonResult) Extract() (*Whitelist, error) {
+	s := &Whitelist{}
+	return s, r.ExtractInto(s)
+}
+
+type CreateResult struct {
+	commonResult
+}
+
+// GetResult represents the result of a get operation.
+type GetResult struct {
+	commonResult
+}
+
+type UpdateResult struct {
+	commonResult
+}
+
+// DeleteResult represents the result of a delete operation.
+type DeleteResult struct {
+	golangsdk.ErrResult
+}
