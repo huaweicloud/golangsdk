2855c668fee18a25e315d2c68ed6232255b0af75
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2020-04-20 14:20:37 +0800

    Update urls.go

--- openstack/cce/v3/addons/urls.go
+++ openstack/cce/v3/addons/urls.go
@@ -11,5 +11,5 @@ func rootURL(client *golangsdk.ServiceClient) string {
 }
 
 func resourceURL(c *golangsdk.ServiceClient, id, cluster_id string) string {
-	return c.ServiceURL(rootPath, id + "?cluster_id=" + cluster_id)
+	return c.ServiceURL(rootPath, id+"?cluster_id="+cluster_id)
 }
