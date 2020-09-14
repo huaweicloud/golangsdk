0eca1f968b8a204536f8442e7f7a9f23e4394cf4
shichangkuo <shichangkuo90@gmail.com>
2020-09-14 20:24:56 +0800

    add acceptance testing for service client (#329)

--- acceptance/openstack/client_test.go
+++ acceptance/openstack/client_test.go
@@ -3,12 +3,14 @@
 package openstack
 
 import (
+	"fmt"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/huaweicloud/golangsdk"
 	"github.com/huaweicloud/golangsdk/openstack"
+	th "github.com/huaweicloud/golangsdk/testhelper"
 )
 
 func TestAuthenticatedClient(t *testing.T) {
@@ -83,4 +85,71 @@ func TestReauth(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Unable to create compute client: %v", err)
 	}
+}
+
+func TestServiceClientEndpoint(t *testing.T) {
+	var err error
+	var serviceClient *golangsdk.ServiceClient
+	var expectedURL, actualURL string
+
+	apiDomain := "myhuaweicloud.com"
+	region := os.Getenv("OS_REGION_NAME")
+	projectID := os.Getenv("OS_PROJECT_ID")
+
+	if projectID == "" {
+		t.Fatalf("OS_PROJECT_ID must be set for service endpoint acceptance test")
+	}
+
+	// Obtain credentials from the environment.
+	ao, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		t.Fatalf("Unable to acquire credentials: %v", err)
+	}
+
+	providerClient, err := openstack.AuthenticatedClient(ao)
+	th.AssertNoErr(t, err)
+
+	if providerClient.TokenID == "" {
+		t.Errorf("No token ID assigned to the client")
+	}
+
+	// Find the cce service in the service catalog.
+	serviceClient, err = openstack.NewCCEV3(providerClient, golangsdk.EndpointOpts{
+		Region: region,
+	})
+	th.AssertNoErr(t, err)
+	expectedURL = fmt.Sprintf("https://cce.%s.%s/api/v3/projects/%s/", region, apiDomain, projectID)
+	actualURL = serviceClient.ResourceBaseURL()
+	th.AssertEquals(t, expectedURL, actualURL)
+	t.Logf("cce endpoint:\t %s", actualURL)
+
+	// Find the ces service in the service catalog.
+	serviceClient, err = openstack.NewCESClient(providerClient, golangsdk.EndpointOpts{
+		Region: region,
+	})
+	th.AssertNoErr(t, err)
+	expectedURL = fmt.Sprintf("https://ces.%s.%s/V1.0/%s/", region, apiDomain, projectID)
+	actualURL = serviceClient.ResourceBaseURL()
+	th.AssertEquals(t, expectedURL, actualURL)
+	t.Logf("ces endpoint:\t %s", actualURL)
+
+	// Find the anti-ddos service in the service catalog.
+	serviceClient, err = openstack.NewHwAntiDDoSV1(providerClient, golangsdk.EndpointOpts{
+		Region: region,
+	})
+	th.AssertNoErr(t, err)
+	expectedURL = fmt.Sprintf("https://antiddos.%s.%s/v1/%s/", region, apiDomain, projectID)
+	actualURL = serviceClient.ResourceBaseURL()
+	th.AssertEquals(t, expectedURL, actualURL)
+	t.Logf("anti-ddos endpoint:\t %s", actualURL)
+
+	// Find the auto-scaling service in the service catalog.
+	serviceClient, err = openstack.NewAutoScalingService(providerClient, golangsdk.EndpointOpts{
+		Region: region,
+	})
+	th.AssertNoErr(t, err)
+	expectedURL = fmt.Sprintf("https://as.%s.%s/autoscaling-api/v1/%s/", region, apiDomain, projectID)
+	actualURL = serviceClient.ResourceBaseURL()
+	th.AssertEquals(t, expectedURL, actualURL)
+	t.Logf("as endpoint:\t %s", actualURL)
 }
