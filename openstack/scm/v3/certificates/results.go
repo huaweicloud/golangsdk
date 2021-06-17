c43a2e8848b25302705f050f6988f7843188181b
ChengXiangdong <chengxiangdong@outlook.com>
2021-06-17 16:46:22 +0800

    fix: AuthentificationList -> Authentifications

--- openstack/scm/v3/certificates/results.go
+++ openstack/scm/v3/certificates/results.go
@@ -18,30 +18,30 @@ type Authentification struct {
 // This struct is used by the API for obtain certificate information and importing.
 type Certificate struct {
 	// the importing API only uses certificate_id.
-	CertificateId        string             `json:"certificate_id"`
-	Id                   string             `json:"id"`
-	Name                 string             `json:"name,omitempty" required:"true"`
-	Certificate          string             `json:"certificate" required:"true"`
-	CertificateChain     string             `json:"certificate_chain" required:"true"`
-	PrivateKey           string             `json:"private_key" required:"true"`
-	Status               string             `json:"status"`
-	OrderId              string             `json:"order_id"`
-	CertificateType      string             `json:"type"`
-	Brand                string             `json:"brand"`
-	PushSupport          string             `json:"push_support"`
-	RevokeReason         string             `json:"revoke_reason"`
-	SignatureAlgrithm    string             `json:"signature_algrithm"`
-	IssueTime            string             `json:"issue_time"`
-	NotBefore            string             `json:"not_before"`
-	NotAfter             string             `json:"not_after"`
-	ValidityPeriod       int8               `json:"validity_period,omitempty"`
-	ValidationMethod     string             `json:"validation_method"`
-	DomainType           string             `json:"domain_type"`
-	Domain               string             `json:"domain"`
-	Sans                 string             `json:"sans"`
-	DomainCount          int8               `json:"domain_count,omitempty"`
-	WildcardCount        int8               `json:"wildcard_count,omitempty"`
-	AuthentificationList []Authentification `json:"authentification,omitempty"`
+	CertificateId     string             `json:"certificate_id"`
+	Id                string             `json:"id"`
+	Name              string             `json:"name,omitempty" required:"true"`
+	Certificate       string             `json:"certificate" required:"true"`
+	CertificateChain  string             `json:"certificate_chain" required:"true"`
+	PrivateKey        string             `json:"private_key" required:"true"`
+	Status            string             `json:"status"`
+	OrderId           string             `json:"order_id"`
+	CertificateType   string             `json:"type"`
+	Brand             string             `json:"brand"`
+	PushSupport       string             `json:"push_support"`
+	RevokeReason      string             `json:"revoke_reason"`
+	SignatureAlgrithm string             `json:"signature_algrithm"`
+	IssueTime         string             `json:"issue_time"`
+	NotBefore         string             `json:"not_before"`
+	NotAfter          string             `json:"not_after"`
+	ValidityPeriod    int8               `json:"validity_period,omitempty"`
+	ValidationMethod  string             `json:"validation_method"`
+	DomainType        string             `json:"domain_type"`
+	Domain            string             `json:"domain"`
+	Sans              string             `json:"sans"`
+	DomainCount       int8               `json:"domain_count,omitempty"`
+	WildcardCount     int8               `json:"wildcard_count,omitempty"`
+	Authentifications []Authentification `json:"authentification,omitempty"`
 }
 
 // CertificatePage is the page returned by a pager when traversing over a
