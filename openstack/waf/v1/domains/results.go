bc59084e8485df0f8f121bdd3c2a7e48226b442b
shichangkuo <shichangkuo90@gmail.com>
2021-06-19 15:59:26 +0800

    waf: update json tag of CertificateId and add delete opts (#488)

--- openstack/waf/v1/domains/results.go
+++ openstack/waf/v1/domains/results.go
@@ -5,47 +5,47 @@ import (
 )
 
 type Domain struct {
-	//Domain ID
+	// Domain ID
 	Id string `json:"id"`
-	//Domain name
+	// Domain name
 	HostName string `json:"hostname"`
-	//Access Code
-	AccessCode string `json:"access_code"`
-	//CNAME value
+	// CNAME value
 	Cname string `json:"cname"`
-	//TXT record
+	// TXT record
 	TxtCode string `json:"txt_code"`
-	//Sub Domain name
+	// Sub Domain name
 	SubDomain string `json:"sub_domain"`
-	//Policy ID
+	// Policy ID
 	PolicyID string `json:"policy_id"`
-	//WAF mode
+	// WAF mode: 0 - disabled, 1 - enabled, -1 - bypassed.
 	ProtectStatus int `json:"protect_status"`
-	//Whether a domain name is connected to WAF
+	// Whether a domain name is connected to WAF
 	AccessStatus int `json:"access_status"`
-	//Protocol type
+	// Protocol type
 	Protocol string `json:"protocol"`
-	//Certificate ID
-	CertificateId string `json:"certificateid"`
+	// Certificate ID
+	CertificateId string `json:"certificate_id"`
 	//The original server information
-	Server []Server `json:"server"`
+	Servers []Server `json:"server"`
 	//Whether proxy is configured
 	Proxy bool `json:"proxy"`
 	//The type of the source IP header
 	SipHeaderName string `json:"sip_header_name"`
 	//The HTTP request header for identifying the real source IP.
 	SipHeaderList []string `json:"sip_header_list"`
+	// the time when the domain is created in unix timestamp
+	TimeStamp int `json:"timestamp"`
 }
 
 type Server struct {
-	//Protocol type of the client
-	ClientProtocol string `json:"client_protocol" required:"true"`
-	//Protocol used by WAF to forward client requests to the server
-	ServerProtocol string `json:"server_protocol" required:"true"`
-	//IP address or domain name of the web server that the client accesses.
-	Address string `json:"address" required:"true"`
-	//Port number used by the web server
-	Port int `json:"port" required:"true"`
+	// Protocol type of the client
+	ClientProtocol string `json:"client_protocol"`
+	// Protocol used by WAF to forward client requests to the server
+	ServerProtocol string `json:"server_protocol"`
+	// IP address or domain name of the web server that the client accesses.
+	Address string `json:"address"`
+	// Port number used by the web server
+	Port int `json:"port"`
 }
 
 type commonResult struct {
