765c44d6c575250ba949e5bc6040679d9f8fbe71
ShiChangkuo <shichangkuo90@gmail.com>
2021-06-21 11:42:23 +0300

    waf: support Get and Update method for alarm masking

--- openstack/waf/v1/falsealarmmasking_rules/requests.go
+++ openstack/waf/v1/falsealarmmasking_rules/requests.go
@@ -16,8 +16,8 @@ type CreateOptsBuilder interface {
 
 // CreateOpts contains all the values needed to create a new falsealarmmasking rule.
 type CreateOpts struct {
-	Url  string `json:"url" required:"true"`
-	Rule string `json:"rule" required:"true"`
+	Path    string `json:"path" required:"true"`
+	EventID string `json:"event_id" required:"true"`
 }
 
 // ToAlarmMaskingCreateMap builds a create request body from CreateOpts.
@@ -37,6 +37,45 @@ func Create(c *golangsdk.ServiceClient, policyID string, opts CreateOptsBuilder)
 	return
 }
 
+// UpdateOptsBuilder allows extensions to add additional parameters to the
+// Update request.
+type UpdateOptsBuilder interface {
+	ToAlarmMaskingUpdateMap() (map[string]interface{}, error)
+}
+
+// UpdateOpts contains all the values needed to update a falsealarmmasking rule.
+type UpdateOpts struct {
+	Path    string `json:"path,omitempty"`
+	EventID string `json:"event_id,omitempty"`
+}
+
+// ToAlarmMaskingUpdateMap builds a update request body from UpdateOpts.
+func (opts UpdateOpts) ToAlarmMaskingUpdateMap() (map[string]interface{}, error) {
+	return golangsdk.BuildRequestBody(opts, "")
+}
+
+// Update accepts a UpdateOpts struct and uses the values to update a rule.The response code from api is 200
+func Update(c *golangsdk.ServiceClient, policyID, ruleID string, opts UpdateOptsBuilder) (r UpdateResult) {
+	b, err := opts.ToAlarmMaskingUpdateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	reqOpt := &golangsdk.RequestOpts{OkCodes: []int{200}}
+	_, r.Err = c.Put(resourceURL(c, policyID, ruleID), b, nil, reqOpt)
+	return
+}
+
+// Get retrieves a particular falsealarmmasking rule based on its unique ID.
+func Get(c *golangsdk.ServiceClient, policyID, ruleID string) (r GetResult) {
+	reqOpt := &golangsdk.RequestOpts{
+		MoreHeaders: RequestOpts.MoreHeaders,
+	}
+
+	_, r.Err = c.Get(resourceURL(c, policyID, ruleID), &r.Body, reqOpt)
+	return
+}
+
 // List retrieves falsealarmmasking rules.
 func List(c *golangsdk.ServiceClient, policyID string) (r ListResult) {
 	_, r.Err = c.Get(rootURL(c, policyID), &r.Body, &RequestOpts)
@@ -45,8 +84,10 @@ func List(c *golangsdk.ServiceClient, policyID string) (r ListResult) {
 
 // Delete will permanently delete a particular falsealarmmasking rule based on its unique ID.
 func Delete(c *golangsdk.ServiceClient, policyID, ruleID string) (r DeleteResult) {
-	reqOpt := &golangsdk.RequestOpts{OkCodes: []int{204},
-		MoreHeaders: RequestOpts.MoreHeaders}
+	reqOpt := &golangsdk.RequestOpts{
+		MoreHeaders: RequestOpts.MoreHeaders,
+	}
+
 	_, r.Err = c.Delete(resourceURL(c, policyID, ruleID), reqOpt)
 	return
 }
--- openstack/waf/v1/falsealarmmasking_rules/urls.go
+++ openstack/waf/v1/falsealarmmasking_rules/urls.go
@@ -2,10 +2,10 @@ package falsealarmmasking_rules
 
 import "github.com/huaweicloud/golangsdk"
 
-func rootURL(c *golangsdk.ServiceClient, policy_id string) string {
-	return c.ServiceURL("policy", policy_id, "ignore")
+func rootURL(c *golangsdk.ServiceClient, policyID string) string {
+	return c.ServiceURL("policy", policyID, "ignore")
 }
 
-func resourceURL(c *golangsdk.ServiceClient, policy_id, id string) string {
-	return c.ServiceURL("policy", policy_id, "ignore", id)
+func resourceURL(c *golangsdk.ServiceClient, policyID, id string) string {
+	return c.ServiceURL("policy", policyID, "ignore", id)
 }
