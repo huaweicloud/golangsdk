8eea1e3cc41f76ef73548fa436c4cc7c2709c7e3
ShiChangkuo <shichangkuo90@gmail.com>
2021-06-21 14:48:33 +0300

    waf: update json tag of PolicyID for response API

--- openstack/waf/v1/whiteblackip_rules/urls.go
+++ openstack/waf/v1/whiteblackip_rules/urls.go
@@ -2,10 +2,10 @@ package whiteblackip_rules
 
 import "github.com/huaweicloud/golangsdk"
 
-func rootURL(c *golangsdk.ServiceClient, policy_id string) string {
-	return c.ServiceURL("policy", policy_id, "whiteblackip")
+func rootURL(c *golangsdk.ServiceClient, policyID string) string {
+	return c.ServiceURL("policy", policyID, "whiteblackip")
 }
 
-func resourceURL(c *golangsdk.ServiceClient, policy_id, id string) string {
-	return c.ServiceURL("policy", policy_id, "whiteblackip", id)
+func resourceURL(c *golangsdk.ServiceClient, policyID, id string) string {
+	return c.ServiceURL("policy", policyID, "whiteblackip", id)
 }
