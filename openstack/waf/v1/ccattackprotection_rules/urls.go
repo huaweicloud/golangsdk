87320bce62de3b0e349b332ee9a135f152f2a2d5
ShiChangkuo <shichangkuo90@gmail.com>
2021-06-23 15:21:26 +0300

    waf: support Update for cc protection rule

--- openstack/waf/v1/ccattackprotection_rules/urls.go
+++ openstack/waf/v1/ccattackprotection_rules/urls.go
@@ -2,10 +2,10 @@ package ccattackprotection_rules
 
 import "github.com/huaweicloud/golangsdk"
 
-func rootURL(c *golangsdk.ServiceClient, policy_id string) string {
-	return c.ServiceURL("policy", policy_id, "cc")
+func rootURL(c *golangsdk.ServiceClient, policyID string) string {
+	return c.ServiceURL("policy", policyID, "cc")
 }
 
-func resourceURL(c *golangsdk.ServiceClient, policy_id, id string) string {
-	return c.ServiceURL("policy", policy_id, "cc", id)
+func resourceURL(c *golangsdk.ServiceClient, policyID, id string) string {
+	return c.ServiceURL("policy", policyID, "cc", id)
 }
