a611c38388d88d4189d3cdcf0bc26c4f20a3982d
ShiChangkuo <shichangkuo90@gmail.com>
2021-06-25 11:16:32 +0300

    waf: update tag of CreateOpts.Url to path

--- openstack/waf/v1/webtamperprotection_rules/urls.go
+++ openstack/waf/v1/webtamperprotection_rules/urls.go
@@ -2,10 +2,10 @@ package webtamperprotection_rules
 
 import "github.com/huaweicloud/golangsdk"
 
-func rootURL(c *golangsdk.ServiceClient, policy_id string) string {
-	return c.ServiceURL("policy", policy_id, "antitamper")
+func rootURL(c *golangsdk.ServiceClient, policyID string) string {
+	return c.ServiceURL("policy", policyID, "antitamper")
 }
 
-func resourceURL(c *golangsdk.ServiceClient, policy_id, id string) string {
-	return c.ServiceURL("policy", policy_id, "antitamper", id)
+func resourceURL(c *golangsdk.ServiceClient, policyID, id string) string {
+	return c.ServiceURL("policy", policyID, "antitamper", id)
 }
