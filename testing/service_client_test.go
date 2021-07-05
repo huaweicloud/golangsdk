gophercloud/gophercloud

add MoreHeaders field to ServiceClient

testing/service_client_test.go
@@ -1,6 +1,8 @@
 package testing
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/gophercloud/gophercloud"
@@ -12,4 +14,21 @@ func TestServiceURL(t *testing.T) {
 	expected := "http://123.45.67.8/more/parts/here"
 	actual := c.ServiceURL("more", "parts", "here")
 	th.CheckEquals(t, expected, actual)
+}
+
+func TestMoreHeaders(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	th.Mux.HandleFunc("/route", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	c := new(gophercloud.ServiceClient)
+	c.MoreHeaders = map[string]string{
+		"custom": "header",
+	}
+	c.ProviderClient = new(gophercloud.ProviderClient)
+	resp, err := c.Get(fmt.Sprintf("%s/route", th.Endpoint()), nil, nil)
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, resp.Request.Header.Get("custom"), "header")
 }

huaweicloud/golangsdk

testing/service_client_test.go
@@ -1,6 +1,8 @@
 package testing
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/huaweicloud/golangsdk"
@@ -12,4 +14,21 @@ func TestServiceURL(t *testing.T) {
 	expected := "http://123.45.67.8/more/parts/here"
 	actual := c.ServiceURL("more", "parts", "here")
 	th.CheckEquals(t, expected, actual)
+}
+
+func TestMoreHeaders(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	th.Mux.HandleFunc("/route", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	c := new(golangsdk.ServiceClient)
+	c.MoreHeaders = map[string]string{
+		"custom": "header",
+	}
+	c.ProviderClient = new(golangsdk.ProviderClient)
+	resp, err := c.Get(fmt.Sprintf("%s/route", th.Endpoint()), nil, nil)
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, resp.Request.Header.Get("custom"), "header")
 }