8d8eb4eee4567f424185fa6bc44855df3f235b95
Lance <74246744+Lance52259@users.noreply.github.com>
2021-07-21 16:14:00 +0800

    add special throttling policy to throttles sdk (#529)

--- openstack/apigw/v2/throttles/urls.go
+++ openstack/apigw/v2/throttles/urls.go
@@ -17,3 +17,11 @@ func rootURL(c *golangsdk.ServiceClient, instanceId string) string {
 func resourceURL(c *golangsdk.ServiceClient, instanceId, policyId string) string {
 	return c.ServiceURL(buildRootPath(instanceId), policyId)
 }
+
+func specRootURL(c *golangsdk.ServiceClient, instanceId, policyId string) string {
+	return c.ServiceURL(buildRootPath(instanceId), policyId, "throttle-specials")
+}
+
+func specResourceURL(c *golangsdk.ServiceClient, instanceId, policyId, strategyId string) string {
+	return c.ServiceURL(buildRootPath(instanceId), policyId, "throttle-specials", strategyId)
+}
