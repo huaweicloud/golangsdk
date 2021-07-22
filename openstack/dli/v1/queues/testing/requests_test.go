083d021079a29f3d4a45376492a2c3fcc75b1e1b
hua ming <hmbig2@163.com>
2021-07-22 11:42:44 +0800

    fix intType default value: when ResourceMode=0 ,still marshal into json string

---
+++ openstack/dli/v1/queues/testing/requests_test.go
@@ -0,0 +1,20 @@
+package testing
+
+import (
+	"testing"
+
+	"github.com/huaweicloud/golangsdk/openstack/dli/v1/queues"
+	th "github.com/huaweicloud/golangsdk/testhelper"
+	"github.com/huaweicloud/golangsdk/testhelper/client"
+)
+
+func TestList(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	handleList(t)
+
+	listResult := queues.List(client.ServiceClient(), queues.ListOpts{})
+	th.AssertNoErr(t, listResult.Err)
+	rt := listResult.Body.(*queues.ListResult)
+	th.AssertDeepEquals(t, expectedListResponseData, rt.Queues[0])
+}
